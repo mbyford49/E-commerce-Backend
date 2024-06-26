1ee376a1a23bd0602adb0163bf53d6e4c721c5af
mbyford49 <mbyford49@gmail.com>
2024-06-25 22:10:42 -0700

    category-routes.js with comments

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -1,28 +1,56 @@
 const router = require('express').Router();
 const { Category, Product } = require('../../models');
 
-// The `/api/categories` endpoint
-
+//The following is a GET request to the root path of the app that will return all categories from the database.
+//The include option will perform a join operation in the database which will return the products associated with each category.
 router.get('/', (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
+  Category.findAll({
+    include: [Product],
+  })
+  .then((categories) => res.json(categories))
+  .catch((err) => res.status(500).json(err));
 });
 
+//The following is a GET request that will return only a single category based on the id in the request.
+//And again the products associated with that single category will be returned as well.
 router.get('/:id', (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
+  Category.findOne({
+    where: {
+      id: req.params.id,
+    },
+    include: [Product],
+  })
+  .then((category) => res.json(category))
+  .catch((err) => res.status(400).json(err));
 });
 
+//The following is a POST request to the root path of the app to create a new category in the database
 router.post('/', (req, res) => {
-  // create a new category
+  Category.create(req.body)
+  .then((category) => res.status(200).json(category))
+  .catch((err) => res.status(400).json(err));
 });
 
+//The following is a PUT request that will update an existing category in the database based on the id in the request
 router.put('/:id', (req, res) => {
-  // update a category by its `id` value
+  Category.update(req.body, {
+    where: {
+      id: req.params.id,
+    },
+  })
+  .then((category) => res.status(200).json(category))
+  .catch((err) => res.status(400).json(err));
 });
 
+//The following is a DELETE request that will delete an existing category in the database based on the id in the request
 router.delete('/:id', (req, res) => {
-  // delete a category by its `id` value
+  Category.destroy({
+    where: {
+      id: req.params.id,
+    }, 
+  })
+  .then((category) => res.status(200).json(category))
+  .catch((err) => res.status(400).json(err));
 });
 
 module.exports = router;
--- Develop/server.js
+++ Develop/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const routes = require('./routes');
-// import sequelize connection
-
+//The follwing imports sequelize from the connection.js file in the config folder
+const sequelize = require('./config/connection');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
@@ -10,7 +10,11 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(routes);
 
-// sync sequelize models to the database, then turn on the server
+//The following will synchronize the sequelize models with the database schema prior to spinning up the server.
+//Note that the force object is set to false to indicate that the exsiting tables in the database
+//should not be dropped and recreated if they already exist
+sequelize.sync({force: false}).then(() => {
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}!`);
+  });
 });
